0a2964db3e4cc48d7bc967db67152a87ecc3a7ea
Netuniii <phchagasaraujo7@gmail.com>
2026-05-28 13:19:29 -0300

    update site

--- app/page.js
+++ app/page.js
@@ -24,8 +24,8 @@ export default function Home() {
       title: "🎮 Gameplay",
       items: [
         { name: "🏰 New Maps", progress: 10 },
-        { name: "⚔️ Combat System", progress: 10 },
-        { name: "🌟 Power System", progress: 30 },
+        { name: "⚔️ Combat System", progress: 20 },
+        { name: "🌟 Power System", progress: 33 },
       ],
     },
     {
